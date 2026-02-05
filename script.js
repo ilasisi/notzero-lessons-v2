371a52fdbb10861601baa50d0d7ee14985c9c3f8
Ibrahim Lasisi <ilasisi90@gmail.com>
2026-02-05 15:08:21 +0100

    Promise and Callback

--- script.js
+++ script.js
@@ -1,149 +1,53 @@
-//Array Methods
+//Callback function
+function greet(name, callback) {
+    console.log("Hello " + name);
 
-const numbers = [1, 2, 55, 3, 5, 6, 7, 11];
-const brands = ["Google", "Meta", "Netflix", "Amazon"];
-const arrayOfArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9, [10, 11, 12]],
-];
-
-//Array.length
-// console.log(numbers.length);
-
-//Array.toString()
-// console.log(numbers.toString());
-
-//Array.at()
-// console.log(brands.at(0));
-
-//Array.join()
-// console.log(numbers.join("|"));
-
-//Array.flat()
-// console.log(arrayOfArray.flat().flat());
-
-//Array.indexOf()
-// console.log(brands.indexOf("Netflix"));
-
-//Array.pop()
-const fruits = ["Mango", "Guava", "Pineapple"];
-// const removedFruit = fruits.pop();
-
-// console.log(fruits);
-// console.log(removedFruit);
-
-//Array.push()
-const fruits2 = ["Banana", "Apple", "Grape"];
-// const newFruitLength = fruits2.push("Orange");
-
-// console.log(fruits2);
-// console.log(newFruitLength);
-
-//Array.shift()
-const fruits3 = ["Strawberry", "Carrot", "Cucumber"];
-// const removedFruit3 = fruits3.shift();
-
-// console.log(fruits3);
-// console.log(removedFruit3);
-//Array.unshift()
-const fruits4 = ["Mango", "Guava", "Pineapple"];
-// const newFruitLength2 = fruits4.unshift("Orange");
-
-// console.log(fruits4);
-// console.log(newFruitLength2);
-
-//Array.concat()
-const newFruits = fruits.concat(fruits2, fruits3);
-
-// console.log(newFruits);
-
-//Array.splice()
-// fruits4[fruits4.length] = "new item";
-fruits4.splice(1, 0, "Banana");
-
-// console.log(fruits4);
-
-//Array.slice()
-
-const carBrands = ["Honda", "Toyota", "Volvo", "Tesla", "Benz"];
-
-const topCarBrand = carBrands.slice(0, 3);
-// console.log(topCarBrand);
-// console.log(carBrands);
-
-//Array.find()
-const states = [
-    { name: "Oyo", index: 1, assigned: true },
-    { name: "Ogun", index: 2, assigned: false },
-    { name: "Lagos", index: 3, assigned: false },
-    { name: "Delta", index: 4, assigned: true },
-    { name: "Oyo", index: 45, assigned: true },
-];
-
-// const state = states.find((state) => state.name === "Oyo");
-
-// console.log(state);
-
-// const toyota = carBrands.find((value) => value === "Toyota");
-// console.log(toyota);
-
-//Array.include()
-// console.log(carBrands.includes("Toyota"));
-
-//Array.some()
-// const oyo = states.some((state) => state.name === "Oyo");
-
-// console.log(oyo);
-
-//Array.every()
-// const assignedStates = states.every((state) => state.assigned === true);
-
-// console.log(assignedStates);
-
-//Array.filter()
-const notAssignedState = states.filter((state) => state.assigned === false);
-
-// console.log(notAssignedState);
-
-//Array.sort()
-
-function compare(a, b) {
-    return a - b;
+    callback();
 }
 
-console.log(numbers.sort(compare));
-console.log(brands.sort());
+greet("Doe", () => console.log("Hello callback"));
 
-//Array.reverse()
-console.log(brands.reverse());
+const date = document.querySelector(".date");
 
-//Array.reduce()
+setInterval(() => {
+    date.textContent = new Date().toLocaleTimeString();
+}, 1000);
 
-function sum(acc, value) {
-    return acc + value;
-}
+//Promises
+const promise = new Promise((resolve, reject) => {
+    const agree = true;
 
-const totalNumber = numbers.reduce(sum, 0);
-
-console.log(totalNumber);
-
-//Array.forEach()
-let total = 0;
-numbers.forEach((number) => {
-    total += number;
+    if (agree) {
+        resolve({ success: true, data: { name: "John" } });
+    } else {
+        reject("Not agree");
+    }
 });
 
-console.log(total);
+promise
+    .then((value) => console.log(value))
+    .catch((error) => console.log(error))
+    .finally(() => console.log("fulfiled"));
 
-//Array.map()
-const newStates = states.map((state) => {
-    return {
-        no: `No: ${state.index}`,
-        nameOfState: state.name,
-        isEligible: state.assigned,
-    };
-});
+const ul = document.querySelector(".list");
+const loading = document.querySelector(".loading");
 
-console.log(newStates);
-console.log(states);
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((response) => {
+        if (response.ok) {
+            response.forEach((post) => {
+                const li = document.createElement("li");
+
+                li.innerHTML = `${post.id}: ${post.title}`;
+
+                ul.appendChild(li);
+            });
+        } else {
+            throw new Error("Error from API");
+        }
+    })
+    .catch((error) => console.log(error.message))
+    .finally(() => {
+        loading.style.display = "none";
+    });
